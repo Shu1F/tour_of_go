0426786280184963cf1b8d25bc3096fc56f93b5f
admin <admin@adminnoMacBook-Pro.local>
2025-07-16 16:09:53 +0900

    finished Basic Section

---
+++ Basics/Types:structs,slices,maps/struct-literals.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Vertex4 struct {
+	X, Y int
+}
+
+var (
+	v1 = Vertex4{1, 2}
+	v2 = Vertex4{X: 2}
+	v3 = Vertex4{}
+	p = &Vertex4{1, 2}
+	q = &v2
+
+)
+
+func main() {
+	fmt.Println(v1, v2, v3, p, q)
+}
